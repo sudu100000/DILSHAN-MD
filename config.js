c24ee7f8280a0ff857aa88305effc7c7268c781c
sudu100000 <dcviral6@gmail.com>
2026-06-08 08:07:51 -0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 
 
 const defaultConfig = {
-  SESSION_ID: "DILSHAN-MD|jk4RVSCR#gZ2wZLc-5eTt07vVeJ6ST-6l3EyTGTj0IfByOzUJ1m0", // Put your session id here
+  SESSION_ID: "DILSHAN-MD|C1AlwZLR#DAuC23XcEwwIW7mRBS6Uwu9lzMCo5gj4oOMiNsqRM9I", // Put your session id here
   ALIVE_IMG: "https://github.com/dilshan62/DILSHAN-MD/blob/main/images/bot_connected.png?raw=true",
   ALIVE_MSG: "", // Change alive msg from here
   BOT_OWNER: "94743376317", // Replace your bot owner number here with 94(country code)
